89e706c379d592716c91f2f615ad45cdb8ea8a6d
Widchard <wf77@drexel.edu>
2019-04-08 20:45:30 -0400

    Started the release of Null-n-Void

---
+++ Null 'n Void/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float horizontalmodifier = 1.0f;
+    public float verticalmodifier = 1.0f;
+    public bool isInAir = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveHorizontal();
+    }
+
+
+    void MoveHorizontal() {
+        Vector3 horizontal = new Vector3((horizontalmodifier * Input.GetAxis("Horizontal")), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+    }
+
+    void MoveVertical() {
+       
+
+            
+    }
+}
